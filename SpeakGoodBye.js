9b1d58f045503f46e481ac9679cd61302839085c
Adabanya Christian <adabanyachristian21@gmail.com>
2024-02-12 23:56:09 +0100

    module5-solution

--- SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-  var byeSpeaker = {};
-  byeSpeaker.speak = function (name) {
-    console.log("Good Bye " + name);
-  };
-
-  window.byeSpeaker = byeSpeaker;
-})(window);
-
--- SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-  var helloSpeaker = {};
-  helloSpeaker.speak = function (name) {
-    console.log("Hello " + name);
-  };
-
-  window.helloSpeaker = helloSpeaker;
-})(window);
